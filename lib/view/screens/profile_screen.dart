10fc1ace1927838f0e536b275ffcb542ce2d78fd
Ardón <josueardon.developer@gmail.com>
2025-04-21 16:11:10 -0600

    fixed profile screen

--- lib/view/screens/profile_screen.dart
+++ lib/view/screens/profile_screen.dart
@@ -52,15 +52,16 @@ class _ProfileScreenState extends State<ProfileScreen> {
   }
 
   Card _education(ProfileViewmodel viewModel) {
+    final education = viewModel.profile.education;
+    final itemCount = education.isNotEmpty ? education.length * 2 - 1 : 0;
+
     return Card(
       elevation: 0,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           spacing: 16.0,
-          children: List.generate(viewModel.profile.education.length * 2 - 1, (
-            index,
-          ) {
+          children: List.generate(itemCount, (index) {
             if (index.isEven) {
               final cardIndex = index ~/ 2;
               return EducationCard(
@@ -76,15 +77,16 @@ class _ProfileScreenState extends State<ProfileScreen> {
   }
 
   Card _experience(BuildContext context, ProfileViewmodel viewModel) {
+    final experiences = viewModel.profile.experience;
+    final itemCount = experiences.isNotEmpty ? experiences.length * 2 - 1 : 0;
+
     return Card(
       elevation: 0,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           spacing: 16.0,
-          children: List.generate(viewModel.profile.experience.length * 2 - 1, (
-            index,
-          ) {
+          children: List.generate(itemCount, (index) {
             if (index.isEven) {
               final cardIndex = index ~/ 2;
               return ExperienceCard(
